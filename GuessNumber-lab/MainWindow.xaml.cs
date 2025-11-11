1e1698b3308252a9b1a571fdedb9493de6951abd
Erik Öberg <erik.oberg@miun.se>
2025-11-11 15:52:07 +0100

    Har fixat - slumpar tal - hämtar in värde från anv - jämför om det är rätt - räknar antal försök

--- GuessNumber-lab/MainWindow.xaml.cs
+++ GuessNumber-lab/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using Accessibility;
 
 namespace GuessNumber_lab
 {
@@ -7,19 +8,65 @@ namespace GuessNumber_lab
     /// </summary>
     public partial class MainWindow : Window
     {
+        // vi skapar en slumpgenerator på detta ställe för att kunna
+        // använda den flera gånger utan att behöva bygga en ny "enarmad bandit"
+        
+        Random _slumpgenerator;
+        int _secretNumber;
+        int _count = 0;
+
+        // konstruktorn körs en enda gång == när programmet startar
         public MainWindow()
         {
             InitializeComponent();
+            _slumpgenerator = new Random();
+            btnGuess.IsEnabled = false;
         }
 
         private void OnNewClick(object sender, RoutedEventArgs e)
         {
-
+             _secretNumber = _slumpgenerator.Next(100) + 1;
+            btnNew.Content = _secretNumber.ToString();
+            btnGuess.IsEnabled = true;
         }
 
         private void OnGuessClick(object sender, RoutedEventArgs e)
         {
+            if (txtGuess.Text == "")
+            {
+                MessageBox.Show("Du ska mata in ett värde");
+                return;
+            }
+
+            if (_secretNumber == 0)
+            {
+                MessageBox.Show("Du måste slumpa ett nytt tal");
+                return;
+            }
+
+            // räkna antalet  gissningar
+            _count++;
+
+
+            // läser in vilket tal användaren har gissat på
+            int guess = int.Parse(txtGuess.Text);
+
+
+            // ta reda på om användaren gissat rätt eller fel
+            if (guess == _secretNumber)
+            {
+                MessageBox.Show($"Grattis, {guess} var rätt men du behöve {_count} försök");
+            }
+            else
+            {
+                MessageBox.Show("Det blev fel");
+            }
+            // om det var fel, hur långt ifrån var jag?
+           
+            // presentera antalet för användaren
 
         }
+
+     
     }
 }
