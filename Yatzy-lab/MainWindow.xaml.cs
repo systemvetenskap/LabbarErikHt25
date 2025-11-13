16cf29fd56815451c6d6a487f3d8cb8748c47319
Erik Öberg <erik.oberg@miun.se>
2025-11-13 14:49:46 +0100

    Yatzy - spara tärningar. - spara värde för ett antal, men inte alla katetgorier

--- Yatzy-lab/MainWindow.xaml.cs
+++ Yatzy-lab/MainWindow.xaml.cs
@@ -10,11 +10,26 @@ namespace Yatzy_lab
         Random _random = new();
         int _rollsLeft = 3;
 
+        // sätt en hakparentes = skapa en array
+        int[] _dices;
+
+        int _total = 0;
+
         public MainWindow()
         {
             InitializeComponent();
             txtRollsLeft.Text = _rollsLeft.ToString();
+            string level = "normal";
 
+            if (level == "maxi")
+            {
+                _dices = new int[7];
+            }
+            else
+            {
+                _dices = new int[5];
+
+            }
             // "⚀⚁⚂⚃⚄⚅"
         }
 
@@ -35,8 +50,7 @@ namespace Yatzy_lab
             // men varje tärning har sex sidor
             //int dice1, dice2, dice3, dice4, dice5;
 
-            // sätt en hakparentes = skapa en array
-            int[] dices = new int[5];
+
             // här har  vi en array of integers med fem fack
 
             // vi måste räkna ner antalet slag som återstår
@@ -45,83 +59,85 @@ namespace Yatzy_lab
             _rollsLeft--;
             txtRollsLeft.Text = _rollsLeft.ToString();
 
-
-            // vi vill kasta tärningarna i en loop
-
-            // första sorten loop = foreach
-            //int counter = 0;
-
-            //foreach (int dice in dices)
-            //{
-            //    dices[counter] = _random.Next(1, 7);
-            //    counter++;
-            //}
-
-            // Den andra loop-sorten är en for
-
-            for (int i = 0; i < dices.Length; i++)
+            bool[] savedDices = { 
+                (bool)chkDiceOne.IsChecked, 
+                (bool)chkDiceTwo.IsChecked ,
+                (bool)chkDiceThree.IsChecked,
+                (bool)chkDiceFour.IsChecked,
+                (bool)chkDiceFive.IsChecked
+               };
+            for (int index = 0; index < _dices.Length; index++)
             {
-                dices[i] = _random.Next(1, 7);
+
+                // Är tärningen sparad?
+                // är = if-sats
+                bool throwDice = !savedDices[index];
+                if (throwDice)
+                {
+                    _dices[index] = _random.Next(1, 7);
+                }
             }
-
-
-
-
-
-
-            // Slumpa värden
-            // dice1 = _random.Next(); // från 0 till 2,147,483,647
-            //dices[0] = _random.Next(1, 7); // från 1 till 6
-            //dices[1] = _random.Next(1, 7); // från 1 till 6
-            //dices[2] = _random.Next(1, 7); // från 1 till 6
-            //dices[3] = _random.Next(1, 7); // från 1 till 6
-            //dices[4] = _random.Next(1, 7); // från 1 till 6
-            ////dice1 = _random.Next(6) + 1;// från 1 till 6
-
-            //dice5 = 3;
-
-            // jag vill säga att tärning nummer 2 ska få värdet 5
-            // 0  1 2 3 4
-            // [][][][][]
-            //int diceValue = _random.Next(1,6);
-            //dices[diceValue - 1] = diceValue;
-
 
             // visa i gränssnittet vad vi fått för resultat
-            diceOne.Text = dices[0].ToString();
-            diceTwo.Text = dices[1].ToString();
-            diceThree.Text = dices[2].ToString();
-            diceFour.Text = dices[3].ToString();
-            diceFive.Text = dices[4].ToString();
-
-            // mål
-            // summera totalvärdet av alla tärningar
-            // = kategori chansen
-            int total = 0;
-            // vi loopar.
-
-            foreach (int dice in dices)
-            {
-                total += dice;
-            }
-
-            // Uppdrag!
-            // vi vill ju kunna spara på ett vist tärningsslag
+            diceOne.Text = _dices[0].ToString();
+            diceTwo.Text = _dices[1].ToString();
+            diceThree.Text = _dices[2].ToString();
+            diceFour.Text = _dices[3].ToString();
+            diceFive.Text = _dices[4].ToString();
         }
 
         private void OnOnesClick(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Ettor klickade");
+            // mål
+            // summera totalvärdet av alla tärningar
+            // där vi bara kikar på ettor
+
+            foreach (int dice in _dices)
+            {
+                if (dice == 1)
+                {
+                    _total += dice;
+                }
+            }
+
+            // int[] dice = { Int32.Parse(diceOne.Text), Int32.Parse(diceTwo.Text), Int32.Parse(diceThree.Text), Int32.Parse(diceFour.Text), Int32.Parse(diceFive.Text) };
+
+            int ones = 0;
+            for (int i = 0; i < _dices.Length; i++)
+            {
+                int dice = _dices[i];
+                if (dice == 1)
+                {
+                    ones += 1;
+                }
+            }
+            txtOnes.Text = ones.ToString();
+
         }
 
         private void OnTwosClick(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Tvåor klickade");
+
+            foreach (int dice in _dices)
+            {
+                if (dice == 2)
+                {
+                    _total += dice;
+                }
+            }
+            txtTwos.Text = _total.ToString();
         }
 
         private void OnThreesClick(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Treor klickade");
+            foreach (int dice in _dices)
+            {
+                if (dice == 3)
+                {
+                    _total += dice;
+                }
+            }
+            txtTwos.Text = _total.ToString();
         }
 
         private void OnFoursClick(object sender, RoutedEventArgs e)
