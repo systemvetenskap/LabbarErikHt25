a10bac876656789c2e5c9f87ec5f41a49f8aa1ea
Erik Öberg <erik.oberg@miun.se>
2025-11-19 11:06:46 +0100

    drink water

--- DrinkWater-lab/MainWindow.xaml.cs
+++ DrinkWater-lab/MainWindow.xaml.cs
@@ -12,11 +12,62 @@ namespace DrinkWater_lab
             InitializeComponent();
         }
 
+        private int CalculateHAtoms(string molekyl)
+        {
+            int numberOfH = 0;
+            char kontrollChar;
+            int molekylLength = molekyl.Length;
+            {
+                for (int i = 0; i < molekylLength; i++)
+                {
+                    kontrollChar = molekyl[i];
+                    if (kontrollChar == 'H' || kontrollChar == 'h')
+                    {
+                        numberOfH = numberOfH + 1;
+                    }
+                }
+                return numberOfH;
+            }
+        }
+
+
         private void OnButtonCalculateClick(object sender, RoutedEventArgs e)
         {
-            string formula = "H2OHO2H7";
+            string formula = "H221222O";
+            formula = "H2O";
+            int count = CountOccurences(formula, 'H');
+
+            if (ContainsValue(formula, 'C'))
+            {
+                MessageBox.Show("Varning, den här drycken är giftig");
+            }
+
+        }
 
 
+        private int CountOccurences(string senctence, char letter)
+        {
+            int count = 0;
+            foreach (char sign in senctence)
+            {
+                if (sign == letter)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private bool ContainsValue(string formula, char atom)
+        {
+            foreach (char letter in formula)
+            {
+                if (char.ToUpper(letter) == char.ToUpper(atom))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
